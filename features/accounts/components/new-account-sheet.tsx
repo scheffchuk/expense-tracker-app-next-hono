6b9b5a27af4772ca6bcd6436c222d90feaf486e6
ScheffDarthusian <darthusian@gmail.com>
2024-12-24 23:03:06 +0900

    created database using NEON and drizzle added create account form

---
+++ features/accounts/components/new-account-sheet.tsx
@@ -0,0 +1,41 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import React from "react";
+import { useNewAccount } from "../hooks/use-new-account";
+import { AccountForm, FormValues } from "./account-form";
+import { useCreateAccount } from "../api/use-create-account";
+
+export const NewAccountSheet = () => {
+  const { isOpen, onClose } = useNewAccount();
+  const mutation = useCreateAccount();
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>
+          <SheetTitle>New Account</SheetTitle>
+          <SheetDescription>
+            Create a new account to track your expense
+          </SheetDescription>
+        </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{ name: "" }}
+        />
+      </SheetContent>
+    </Sheet>
+  );
+};
